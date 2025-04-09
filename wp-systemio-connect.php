77c824365db4e3d3e437b21d857f51b58578e055
WakamTech <williammerveilleaklamavo@gmail.com>
2025-04-09 10:51:17 +0100

    Création de la Structure de Base du Plugin

--- wp-systemio-connect.php
+++ wp-systemio-connect.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Plugin Name:       WP Systeme.io Connect
- * Plugin URI:        https://example.com/plugin-uri/  <-- Met ton URL si tu en as une
+ * Plugin URI:        https://github.com/WakamTech/wp-systemio-connect  
  * Description:       Connecte les formulaires WordPress à Systeme.io pour une gestion facile des emails.
  * Version:           0.1.0
- * Author:            Ton Nom ou Nom de ton entreprise
- * Author URI:        https://example.com/author-uri/ <-- Met ton URL si tu en as une
+ * Author:            BO-VO Digital
+ * Author URI:        https://bovo-digital.tech/ 
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       wp-systemio-connect
@@ -17,9 +17,7 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly
 }
 
-// Pour l'instant, ce fichier ne fait rien de plus,
-// mais l'en-tête ci-dessus est essentielle.
-// Nous ajouterons le code fonctionnel ici plus tard.
+
 
 echo "<!-- WP Systeme.io Connect est chargé (version basique) -->"; // Juste pour vérifier qu'il se charge, on enlèvera ça plus tard
 
